6dde2b4e44a8b4f63464c7c04bd168401b2f0c6a
madhuryashankar <64582642+madhuryashankar@users.noreply.github.com>
2023-09-30 17:53:46 -0400

    Update hw4.py

--- hw4.py
+++ hw4.py
@@ -1,21 +1,35 @@
 import streamlit as st
+import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
 
-# Load the dataset
-@st.cache_data
-def load_data():
-    df = pd.read_csv('https://raw.githubusercontent.com/madhuryashankar/CMSE/main/healthcare-dataset-stroke-data.csv')  # Adjust the file name if needed
-    return df
+# Keep this to avoid unwanted warning on the wen app
+st.set_option('deprecation.showPyplotGlobalUse', False)
 
-df = load_data()
-st.title('Stroke Prediction Data Set Explorer')
-df
+# Loading dataset
+data = pd.read_csv('https://raw.githubusercontent.com/madhuryashankar/CMSE/main/healthcare-dataset-stroke-data.csv')
+X = data.drop('stroke',axis =1)
+y = data['stroke']
+print(data.columns.tolist())
+# Title 
+st.title('Stroke Prediction Dataset Exploration')
+st.write('Interactive plots on Data')
 
-# User input to select a column for plotting
-selected_column = st.selectbox('Select a column for plotting', df.columns)
+# Sidebar
+st.sidebar.title('Control Panel')
 
-# Plot using Seaborn
-st.subheader(f'KDE Plot for {selected_column}')
-plot = sns.kdeplot(data = df, x = df[selected_column])
-st.pyplot(fig=plot.get_figure())
+# Interactive Plots
+st.sidebar.subheader('Select a feature for the histogram:')
+selected_feature = st.sidebar.selectbox('Select a feature', X.columns.tolist())
+bin_count = st.sidebar.slider('Number of Bins', min_value=1, max_value=100, value=20)
+st.subheader(f'Histogram of {selected_feature}')
+sns.histplot(data= data, x= data[selected_feature], hue='stroke', kde=True, bins=bin_count)
+st.pyplot()
+
+st.write(pd.DataFrame(data, columns=data.columns))
+
+st.write('''
+This Streamlit app allows the user to explore the stroke prediction dataset using interactive histogram plot.
+You can choose the type of plot and features to visualize in the sidebar on the left.
+You can also adjust the number of bins for histogram plot.
+''')
