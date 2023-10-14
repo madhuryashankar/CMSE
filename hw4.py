aff83e7925ec12793207ab5c79af9b5fa54fafe2
madhuryashankar <64582642+madhuryashankar@users.noreply.github.com>
2023-10-13 21:47:05 -0400

    Update hw4.py

--- hw4.py
+++ hw4.py
@@ -26,6 +26,38 @@ st.subheader(f'Histogram of {selected_feature}')
 sns.histplot(data= data, x= data[selected_feature], hue='stroke', kde=True, bins=bin_count)
 st.pyplot()
 
+# Data Visualization
+st.subheader("Data Visualization")
+
+# Add interactive charts or plots using Altair
+# Example: Scatter plot of age vs. avg_glucose_level
+scatter = alt.Chart(data).mark_circle().encode(
+    x='age',
+    y='avg_glucose_level',
+    color='stroke:N'
+).interactive()
+
+st.altair_chart(scatter, use_container_width=True)
+
+# Bar chart of stroke counts
+st.subheader("Bar charts of stroke counts")
+stroke_counts = data['stroke'].value_counts().reset_index()
+stroke_counts.columns = ['Stroke', 'Count']
+bar_chart = alt.Chart(stroke_counts).mark_bar().encode(
+    x='Stroke:N',
+    y='Count:Q',
+    color=alt.condition(
+        alt.datum.Stroke == '1',
+        alt.value("steelblue"),
+        alt.value("lightgray")
+    )
+).properties(
+    width=300,
+    height=300
+).interactive()
+
+st.altair_chart(bar_chart, use_container_width=True)
+
 st.write(pd.DataFrame(data, columns=data.columns))
 
 st.write('''
