e5aaeea7e33b3f859eb4d189d6783e4a06b726a7
madhuryashankar <64582642+madhuryashankar@users.noreply.github.com>
2023-12-06 09:25:24 -0500

    Update stroke.py

--- stroke.py
+++ stroke.py
@@ -5,6 +5,33 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 import hiplot as hip
+import time as timer
+import joblib
+
+from sklearn import metrics
+from imblearn.over_sampling import SMOTE
+from sklearn.model_selection import train_test_split
+from sklearn.inspection import permutation_importance
+from sklearn.preprocessing import LabelEncoder,StandardScaler
+from sklearn.metrics import classification_report,accuracy_score,confusion_matrix
+from sklearn.metrics import auc,roc_auc_score,roc_curve,precision_score,recall_score,f1_score
+
+
+#model imports
+from sklearn.svm import SVC
+from sklearn.naive_bayes import GaussianNB
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import cross_val_score
+from sklearn.ensemble import RandomForestClassifier
+from xgboost import XGBClassifier
+from xgboost import plot_importance
+
+#charts
+import plotly.express as px
+import matplotlib.pyplot as plt
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
 
 # Keep this to avoid unwanted warning on the wen app
 st.set_option('deprecation.showPyplotGlobalUse', False)
@@ -65,7 +92,7 @@ st.set_page_config(
 st.write('<h2 style="text-align:center; vertical-align:middle; line-height:2; color:#046366;">Predicting Strokes: Insights from the Data</h2>', unsafe_allow_html=True)
 
 # Create tabs
-tab1, tab2, tab3, tab4 = st.tabs(["About the Data", "Visualizations", "Playground", "Conclusion"])
+tab1, tab2, tab3, tab4, tab5, tab6, tab7 = st.tabs(["About the Data", "Visualizations", "Playground", "Insights", "Method Assessment", "Prediciton", "About Me"])
 
 with tab1 :
     image_path = "bg.png"  # Replace with the actual file path
@@ -322,5 +349,250 @@ with tab4 :
     st.markdown("4. The analysis also indicated that bmi might not be a strong predictor for stroke, as the distribution of BMI was similar for stroke and non-stroke patients.")
     st.markdown("5. It was observed that older patients, particularly those who are self-employed or in private jobs, have a higher incidence of stroke. Also, stroke patients generally have higher glucose levels regardless of their work type and gender.")
     st.markdown("6. The EDA provided valuable insights into the factors associated with strokes. Age, hypertension, heart disease, and average glucose level appear to be significant factors, while BMI might not be a significant predictor. This information can guide the feature selection and modeling process. However, the imbalance in the target variable could present a challenge in building a predictive model.")
-    st.markdown("References")
     st.markdown("[Stroke Prediction Dataset](https://www.kaggle.com/fedesoriano/stroke-prediction-dataset)")
+
+with tab5 :
+        label_gender = LabelEncoder()
+        label_married = LabelEncoder()
+        label_work = LabelEncoder()
+        label_residence = LabelEncoder()
+        label_smoking = LabelEncoder()
+        df['gender'] = label_gender.fit_transform(df['gender'])
+        df['ever_married'] = label_married.fit_transform(df['ever_married'])
+        df['work_type']= label_work.fit_transform(df['work_type'])
+        df['Residence_type']= label_residence.fit_transform(df['Residence_type'])
+        df['smoking_status']= label_smoking.fit_transform(df['smoking_status'])
+         #Handling Imbalanced Class Data Using SMOTE Technique
+        smote = SMOTE(sampling_strategy='minority')
+        X, y= smote.fit_resample(df.loc[:,df.columns!='stroke'], df['stroke'])
+        #Building Data Model and Training
+        X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.22,random_state=42)
+
+        #Data Standardization
+        scaler = StandardScaler()
+        scaler = scaler.fit(X_train)
+        X_train_std = scaler.transform(X_train)
+        X_test_std = scaler.transform(X_test)
+
+        #ML Model Training and Evaluation
+        model_menu = ["XGBoost (XGB) with HyperTuned Parameters","XGBoost (XGB)","Random Forest (RF)","Logistic Regression (LR)","Decision Tree (DT)","Gaussian Naive Bayes (GNB)","Singular Vector Machine (SVM)"]
+        model = st.selectbox("Select a Model",model_menu)
+        #XGBoost
+        if model == "XGBoost (XGB)":
+            start = timer.time()
+            xgb_m = XGBClassifier(objective="reg:logistic", random_state=42,use_label_encoder = False)
+            xgb_m.fit(X_train, y_train)
+            end = timer.time()
+            st.success("Training time {:.2f} seconds".format(end-start))
+            # Predicting test data
+            y_xgb = xgb_m.predict(X_test)
+            cnf_matrix = metrics.confusion_matrix(y_test, y_xgb)
+            st.write("Confusion matrix:\n",cnf_matrix)
+            st.write("Accuracy:",metrics.accuracy_score(y_test, y_xgb))
+            st.write("Precision:",metrics.precision_score(y_test, y_xgb))
+            st.write("Recall:",metrics.recall_score(y_test, y_xgb))
+            st.write("F1:",metrics.f1_score(y_test, y_xgb))
+        #XGBoost with HyperTuned Parameter
+        elif model == "XGBoost (XGB) with HyperTuned Parameters":
+            start = timer.time()
+            xgb_mt = XGBClassifier(objective="reg:logistic", random_state=42,
+                                use_label_encoder = False, colsample_bytree= 0.5, 
+                                gamma= 0.2, learning_rate= 0.25,
+                                max_depth= 10, min_child_weight= 1,)
+            xgb_mt.fit(X_train, y_train)
+            end = timer.time()
+            st.success("Training time {:.2f} seconds".format(end-start))
+            # Predicting test data
+            y_xgb = xgb_mt.predict(X_test)
+            y_train_predict = xgb_mt.predict(X_train)
+            cnf_matrix = metrics.confusion_matrix(y_train , y_train_predict)
+            st.write("Confusion matrix:\n",cnf_matrix)
+            st.write('Train Accuracy',accuracy_score(y_train , y_train_predict))
+            st.write("Accuracy:",metrics.accuracy_score(y_test, y_xgb))
+            st.write("Precision:",metrics.precision_score(y_test, y_xgb))
+            st.write("Recall:",metrics.recall_score(y_test, y_xgb))
+            st.write("F1:",metrics.f1_score(y_test, y_xgb))
+        # Random Forest
+        elif model == "Random Forest (RF)":
+            start = timer.time()
+            ranfor_m = RandomForestClassifier(n_estimators=100, random_state=42)
+            ranfor_m.fit(X_train, y_train)
+            end = timer.time()
+            st.success("Training time {:.2f} seconds".format(end-start))
+            # Predicting test data
+            y_ranfor = ranfor_m.predict(X_test)
+            cnf_matrix = metrics.confusion_matrix(y_test, y_ranfor)
+            st.write("Confusion matrix:\n",cnf_matrix)
+            st.write("Accuracy:",metrics.accuracy_score(y_test, y_ranfor))
+            st.write("Precision:",metrics.precision_score(y_test, y_ranfor))
+            st.write("Recall:",metrics.recall_score(y_test, y_ranfor))
+            st.write("F1:",metrics.f1_score(y_test, y_ranfor))
+        # Decision Tree
+        elif model == "Decision Tree (DT)":
+            start = timer.time()
+            dtree_m = DecisionTreeClassifier(random_state=42)
+            dtree_m.fit(X_train, y_train)
+            end = timer.time()
+            st.success("Training time {:.2f} seconds".format(end-start))
+            # Predicting test data
+            y_dtree = dtree_m.predict(X_test)
+            cnf_matrix = metrics.confusion_matrix(y_test, y_dtree)
+            st.write("Confusion matrix:\n",cnf_matrix)
+            st.write("Accuracy:",metrics.accuracy_score(y_test, y_dtree))
+            st.write("Precision:",metrics.precision_score(y_test, y_dtree))
+            st.write("Recall:",metrics.recall_score(y_test, y_dtree))
+            st.write("F1:",metrics.f1_score(y_test, y_dtree))
+        # Logistic Regression
+        elif model == "Logistic Regression (LR)":
+            start = timer.time()
+            logit_m = LogisticRegression(solver='lbfgs', random_state=42)
+            logit_m.fit(X_train_std,y_train)
+            end = timer.time()
+            st.success("Training time {:.2f} seconds".format(end-start))
+            # Predicting test data
+            y_pred = logit_m.predict(X_test_std)
+            cnf_matrix = metrics.confusion_matrix(y_test, y_pred)
+            st.write("Confusion matrix:\n",cnf_matrix)
+            st.write("Accuracy:",metrics.accuracy_score(y_test, y_pred))
+            st.write("Precision:",metrics.precision_score(y_test, y_pred))
+            st.write("Recall:",metrics.recall_score(y_test, y_pred))
+            st.write("F1:",metrics.f1_score(y_test, y_pred))
+        # Gaussian Naive Bayes
+        elif model == "Gaussian Naive Bayes (GNB)":
+            start = timer.time()
+            gnb_m = GaussianNB()
+            gnb_m.fit(X_train, y_train)
+            end = timer.time()
+            st.success("Training time {:.2f} seconds".format(end-start))
+            # Predicting test data
+            y_gnb = gnb_m.predict(X_test)
+            cnf_matrix = metrics.confusion_matrix(y_test, y_gnb)
+            st.write("Confusion matrix:\n",cnf_matrix)
+            st.write("Accuracy:",metrics.accuracy_score(y_test, y_gnb))
+            st.write("Precision:",metrics.precision_score(y_test, y_gnb))
+            st.write("Recall:",metrics.recall_score(y_test, y_gnb))
+            st.write("F1:",metrics.f1_score(y_test, y_gnb))
+        # Singular Vector Machine
+        elif model == "Singular Vector Machine (SVM)":
+            start = timer.time()
+            svm_m = SVC(kernel='rbf',probability=True)
+            svm_m.fit(X_train_std, y_train)
+            end = timer.time()
+            st.success("Training time {:.2f} seconds".format(end-start))
+            # Predicting test data
+            y_svm = svm_m.predict(X_test_std)
+            cnf_matrix = metrics.confusion_matrix(y_test, y_svm)
+            st.write("Confusion matrix:\n",cnf_matrix)
+            st.write("Accuracy:",metrics.accuracy_score(y_test, y_svm))
+            st.write("Precision:",metrics.precision_score(y_test, y_svm))
+            st.write("Recall:",metrics.recall_score(y_test, y_svm))
+            st.write("F1 Score:",metrics.f1_score(y_test, y_svm))
+        
+        st.info("XGBoost (with Hyper Tuned Parameters) has been selected as the Best Model due to its High Accuracy compared to other models that has been Trained")
+
+with tab6 :
+
+        st.markdown("Enter the User's Details to predict the occurance of Stroke")
+        st.text("Please Enter correct details to get better results")
+        
+        #Getting User Inputs
+        gender = st.radio("What is User's gender",("Male","Female"))
+        age = st.number_input("Enter User's age",value=40)
+        hypertension = st.radio("Hypertension?",("Yes","No"))
+        heart_disease = st.radio("User Ever had a heart disease?",("Yes","No"))
+        ever_married = st.radio("User Ever Married?",("Yes","No"))
+        work_type = st.radio("What is User's work type?",("Government Job","Private Job","Self Employed","Never Worked","Children"))
+        Residence_type = st.radio("What is User's Residence type?",("Urban","Rural"))
+        avg_glucose_level = st.number_input("Enter User's Average Glucose Level",value=92.35)
+        
+        #BMI Calculation with Height and Weight is User doesn't know BMI
+        if st.checkbox("Dont Know BMI? Use height and weight"):
+            height = st.number_input("Enter User's Height in cm",value=160)
+            weight = st.number_input("Enter User's Weight in kgs",value=60)
+            bmi = weight / (height/100)**2
+            st.write("BMI of user is {:.2f} and will be autoupdated".format(bmi))
+        else:
+            bmi = st.number_input("Enter User's BMI",value=25.4)
+
+        smoking_status = st.radio("User's Smoking Status?",("Unknown","Formerly Smoked","Never Smoked","Smokes"))
+        
+        #model (XGBoost)
+        prediction_model = 'XGBoost'
+        trained_model = joblib.load('Models/XGBoostTunedModel.pkl')
+        model_accuracy = "94.9%"
+
+        if st.button("Submit"):
+            #Encoding categorical attributes to values
+            gender = 1 if gender == 'Male' else 0
+            age = float(age)
+            hypertension = 1 if hypertension == 'Yes' else 0
+            ever_married = 1 if ever_married == 'Yes' else 0
+            heart_disease = 1 if heart_disease == 'Yes' else 0
+            if work_type == 'Government Job':
+                work_type = 0 
+            elif work_type == 'Never Worked':
+                work_type = 1 
+            elif work_type == 'Private Job':
+                work_type = 2 
+            elif work_type == 'Self Employed':
+                work_type = 3
+            elif work_type == 'Children':
+                work_type = 4 
+            Residence_type = 1 if Residence_type == 'Urban' else 0
+            avg_glucose_level = float(avg_glucose_level)
+            bmi = float(bmi)
+            if smoking_status == 'Unknown':
+                smoking_status = 0 
+            elif smoking_status == 'Formerly Smoked':
+                smoking_status = 1 
+            elif smoking_status == 'Never Smoked':
+                smoking_status = 2
+            elif smoking_status == 'Smokes':
+                smoking_status = 3 
+
+            #Creating nparray of User Inputs
+            user_input = np.array([gender,age,hypertension,heart_disease,ever_married,work_type,Residence_type,avg_glucose_level,bmi,smoking_status]).reshape(1,-1)
+            
+            #converting into dataframe to avoid mismatching feature_names error
+            user_input = pd.DataFrame(user_input, columns = ['gender', 'age', 'hypertension', 'heart_disease', 'ever_married', 'work_type', 'Residence_type', 'avg_glucose_level', 'bmi', 'smoking_status'])
+            
+            #prediction using selected model
+            prediction = trained_model.predict(user_input)
+
+            #Prediction Probability
+            pred_prob = trained_model.predict_proba(user_input)
+            stroke_prob = pred_prob[0][1]*100
+
+            #Printing Predicted results
+            if prediction == 1:
+                st.header("User has Higher Chances of having a Stroke")
+            else:
+                st.header("User has Lower Chances of having a Stroke")
+            
+            #printing prediction probability 
+            if stroke_prob < 25:
+                st.success("Probability of Occurance of Stroke is {:.2f}%".format(stroke_prob))
+            elif stroke_prob < 50:
+                st.info("Probability of Occurance of Stroke is {:.2f}%".format(stroke_prob))
+            elif stroke_prob < 75:
+                st.warning("Probability of Occurance of Stroke is {:.2f}%".format(stroke_prob))
+            else:
+                st.error("Probability of Occurance of Stroke is {:.2f}%".format(stroke_prob))
+            st.text("Predicted with "+prediction_model+" Model with Accuracy of " +model_accuracy)
+
+with tab7 :
+
+    image_path = "bio.jpg" 
+    image = open(image_path, "rb").read()
+    st.image(image, width=200)
+    text_column = st.columns(2)[0]
+
+    with text_column:
+        st.write("Hello there! I'm Madhurya, a dedicated learner currently pursuing a Master's in Data Science. My journey is fueled by a passion for unraveling the stories hidden in data.")
+        st.write("In the halls of MSU, I dive deep into the realms of Python, Data Analysis, and Machine Learning. Learning isn't just a task; it's my enthusiasm for embracing new technologies and methodologies in the dynamic field of data science.") 
+        st.write("Beyond the screen, I find joy in diverse pursuits. Whether it's a fierce badminton match, the strokes of a paintbrush, the soothing chords of a guitar, or the tranquility of a hiking trail, I embrace the beauty of life beyond coding.")
+        st.write("Come, explore my web app, and join me in this exciting adventure of data exploration and analytics. Let's make technology not just a skill but a thrilling journey!")
+
+
+
+
